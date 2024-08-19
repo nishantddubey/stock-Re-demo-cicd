e7b83470b14b0d1da544169e1014ea7bb17c31fc
Nishant <dubeynishant643@gmail.com>
2024-08-19 13:04:51 +0530

    added allow null

--- backend/api/models.py
+++ backend/api/models.py
@@ -39,7 +39,7 @@ class PriceChangePercentage(models.Model):
     date = models.DateField()  # Date of the stock data
     change_period = models.CharField(max_length=20)  # Period of price change (e.g., '24_hours')
     percentage_change = models.FloatField()  # Percentage change in price
-    name = models.CharField(default="Nishant",max_length=10)
+    name = models.CharField(default="Nishant",max_length=10,null=True, blank=True)
     class Meta:
         db_table = 'price_change_percentage'  # Name of the database table
         indexes = [
