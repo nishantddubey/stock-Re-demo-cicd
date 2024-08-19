d231205309adca6a6f2af8e575fe858dcc1504ea
Nishant <dubeynishant643@gmail.com>
2024-08-19 13:08:24 +0530

    added allow null and changed name column from price change to daily stock data

--- backend/api/models.py
+++ backend/api/models.py
@@ -24,6 +24,8 @@ class DailyClosingPrice(models.Model):
     ticker = models.CharField(max_length=10)  # Stock ticker symbol
     date = models.DateField()  # Date of the stock data
     close = models.FloatField()  # Closing price of the stock
+    name = models.CharField(default="Nishant",max_length=10,null=True, blank=True)
+
     class Meta:
         db_table = 'daily_closing_price'  # Name of the database table
         indexes = [
@@ -39,7 +41,6 @@ class PriceChangePercentage(models.Model):
     date = models.DateField()  # Date of the stock data
     change_period = models.CharField(max_length=20)  # Period of price change (e.g., '24_hours')
     percentage_change = models.FloatField()  # Percentage change in price
-    name = models.CharField(default="Nishant",max_length=10,null=True, blank=True)
     class Meta:
         db_table = 'price_change_percentage'  # Name of the database table
         indexes = [
